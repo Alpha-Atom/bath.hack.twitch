70aa2bf526e3e23847bc5cd0a7637d01ed7d04a6
Daniel Makepeace-Lane <Variant@138-38-236-53.eduroam.bath.ac.uk>
2015-11-14 22:58:43 +0000

    Added mode switching

--- check_valid.js
+++ check_valid.js
@@ -1,6 +1,17 @@
 var command_regex = /(up|down|left|right|[1-9]|delete|anarchy|democracy)/;
+var votes = { 
+  "anarchy":   10,
+  "democracy": 10,
+};
 module.exports = {
-  check_valid_format_command: function (message) {
+  mode: false,
+  users: [],
+
+  reset_user_votes_for_mode: function() {
+    this.users = [];
+  }
+
+  check_valid_format_command: function (message, user) {
     var command_type = message.match(command_regex);
     if (command_type !== null) {
       command_type = command_type[0];
@@ -21,10 +32,24 @@ module.exports = {
           command_type = "D";
         break;
         case "anarchy":
-          command_type = "modea";
+          if (!~this.users.indexOf(user['display-name'])) {
+            votes["anarchy"] += 1;
+            this.users.push(user['display-name']);
+            console.log(votes["anarchy"]);
+            if (((anarchy - democracy) / (anarchy + democracy)) > 0.6) {
+              this.mode = false;
+            }
+          }
         break;
         case "democracy":
-          command_type = "moded";
+          if (!~this.users.indexOf(user['display-name'])) {
+            votes["democracy"] += 1;
+            this.users.push(user['display-name']);
+            console.log(votes["democracy"]);
+            if (((democracy - anarchy) / (anarchy + democracy)) > 0.8) {
+              this.mode = true;
+            }
+          }
         break;
         default:
           break;
--- main.js
+++ main.js
@@ -4,7 +4,6 @@ var checker   = require("./check_valid.js");
 var democracy = require("./democracy.js");
 var anarchy   = require("./anarchy.js");
 var MILLIS    = 1000;
-var mode      = false;
 var position  = {
   value: 00
 };
@@ -31,16 +30,15 @@ try {
 fs.writeFileSync('res/command_list.txt', '');
 
 var client = new irc.client(options);
-var toggle_mode = function() { mode = !mode; };
 
 // Connect the client to the server..
 client.connect();
 client.on("chat", function (channel, user, message, self) {
   var message_formatted;
   if (self === false) {
-    message_formatted = checker.check_valid_format_command(message);
+    message_formatted = checker.check_valid_format_command(message, user);
     if (message_formatted["valid"] === true) {
-      if (mode === true) {
+      if (checker.mode === true) {
         console.log("Command Processed: " + message_formatted["content"]);
         democracy.write(user, message_formatted["content"], position);
       } else {
@@ -51,3 +49,4 @@ client.on("chat", function (channel, user, message, self) {
   }
 });
 setInterval(function(){democracy.process(client, mode, postion)}, 15*MILLIS);
+setInterval(function(){checker.reset_user_votes_for_mode()}, 60*MILLIS);
