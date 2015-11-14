8885843bf1c2604f4b409fb99a7d2cf8074e15f6
Matt <mattc65@gmail.com>
2015-11-14 18:36:21 +0000

    fuck

--- java/src/FileListener.java
+++ java/src/FileListener.java
@@ -6,44 +6,44 @@ import java.io.BufferedReader;
 import java.io.IOException;
 
 class FileListener extends Thread{
-	SudokuJavaFx app;
+  SudokuJavaFx app;
 
-	public void setApp(SudokuJavaFx newApp){
-		app = newApp;
-	}
+  public void setApp(SudokuJavaFx newApp){
+    app = newApp;
+  }
 
-	public void run() {
-		//File reading and game loop
-		try {
-			File file = new File("./command_list.txt");
-			FileInputStream fr = new FileInputStream(file);
-			BufferedInputStream br = new BufferedInputStream(fr);
-			String line ="";
-			while(true) {
+  public void run() {
+    //File reading and game loop
+    try {
+      File file = new File("./res/command_list.txt");
+      FileInputStream fr = new FileInputStream(file);
+      BufferedInputStream br = new BufferedInputStream(fr);
+      String line ="";
+      while(true) {
 
-				if(br.available() > 0){
-					char readChar = (char) br.read();
-					System.out.println("Reading Char: " + Character.toString(readChar));
-					if(readChar != '\n')
-						line += (Character.toString(readChar));
-					else {
-						app.handleCommand(line);
-						line = "";
-						try {
-							Thread.sleep(1000);
-						} catch (InterruptedException e) {
-							e.printStackTrace();
-							break;
-						}
-					}
-				}
-			}
-			br.close();
-			fr.close();
-		}  catch (IOException e) {
-			System.err.println("Caught IOException: " + e.getMessage());
-		}
-	}
+        if(br.available() > 0){
+          char readChar = (char) br.read();
+          System.out.println("Reading Char: " + Character.toString(readChar));
+          if(readChar != '\n')
+            line += (Character.toString(readChar));
+          else {
+            app.handleCommand(line);
+            line = "";
+            try {
+              Thread.sleep(1000);
+            } catch (InterruptedException e) {
+              e.printStackTrace();
+              break;
+            }
+          }
+        }
+      }
+      br.close();
+      fr.close();
+    }  catch (IOException e) {
+      System.err.println("Caught IOException: " + e.getMessage());
+    }
+  }
 }
 
 
