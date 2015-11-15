04e770dc2b4cb63113bb29dcae14d1ac6a613769
Matt <mattc65@gmail.com>
2015-11-15 01:07:45 +0000

    fuck node's shitty readfile

--- gameboard.js
+++ gameboard.js
@@ -11,7 +11,7 @@ module.exports = {
   loadGameboard: function () {
     var board_tmp;
     if (this.checkGame() === true) {
-      board_tmp = fs.readFileSync(gameboard_name + ".txt");
+      board_tmp = fs.readFileSync(gameboard_name + ".txt") + "";
       gameboard = board_tmp.split("\n");
       solution = fs.readFileSync(gameboard_name + "solution.txt").split("\n");
       leaderboard.saveToFile();
