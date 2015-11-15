74545c31c29ce388bc2f1d983ff8497a9bc9d35b
Matt <mattc65@gmail.com>
2015-11-15 00:34:31 +0000

    remove garbage encoding options

--- gameboard.js
+++ gameboard.js
@@ -11,15 +11,15 @@ module.exports = {
   loadGameboard: function () {
     var board_tmp;
     if (this.checkGame() === true) {
-      board_tmp = fs.readFileSync(gameboard_name + ".txt", 'utf-8');
+      board_tmp = fs.readFileSync(gameboard_name + ".txt");
       gameboard = board_tmp.split("\n");
-      solution = fs.readFileSync(gameboard_name + "solution.txt", 'utf-8').split("\n");
+      solution = fs.readFileSync(gameboard_name + "solution.txt").split("\n");
       leaderboard.saveToFile();
     }
   },
 
   checkGame: function () {
-    var name_tmp = fs.readFileSync("./res/current_game.txt", 'utf-8');
+    var name_tmp = fs.readFileSync("./res/current_game.txt");
     if (name_tmp !== gameboard_name) {
       gameboard_name = name_tmp;
       return true;
