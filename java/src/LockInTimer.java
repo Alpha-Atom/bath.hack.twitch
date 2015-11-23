305eb2b9838beef69ee72a70b30832feb5e8a368
Matt <mattc65@gmail.com>
2015-11-23 12:26:02 +0000

    fix indentation lel

--- java/src/LockInTimer.java
+++ java/src/LockInTimer.java
@@ -1,24 +1,24 @@
 
 public class LockInTimer extends Thread{
-    private SudokuJavaFx application;
-    private int timerMilliseconds;
-    
-    public LockInTimer(int milliseconds, SudokuJavaFx app){
-        timerMilliseconds = milliseconds;
-        application = app;
+  private SudokuJavaFx application;
+  private int timerMilliseconds;
+
+  public LockInTimer(int milliseconds, SudokuJavaFx app){
+    timerMilliseconds = milliseconds;
+    application = app;
+  }
+
+  public void run(){
+    try {
+      this.sleep(timerMilliseconds);
+      application.lockIn();
+    } catch (InterruptedException e) {
+      e.printStackTrace();
     }
-    
-    public void run(){
-        try {
-            this.sleep(timerMilliseconds);
-            application.lockIn();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        
-    }
-    
-    public void updateApp(SudokuJavaFx app){
-        application = app;
-    }
+
+  }
+
+  public void updateApp(SudokuJavaFx app){
+    application = app;
+  }
 }
